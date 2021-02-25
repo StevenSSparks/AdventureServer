6a122be5caa4be1a1cae1702c470dae07a0a6f09
Steve Sparks <>
2021-02-24 21:28:50 -0600

    Added Dev Exceptions

--- AdventureServer/Startup.cs
+++ AdventureServer/Startup.cs
@@ -49,10 +49,9 @@ namespace AdventureServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+  
+            app.UseDeveloperExceptionPage();
+            
 
             app.UseHttpsRedirection();
 
@@ -63,11 +62,6 @@ namespace AdventureServer
             // app.UseAuthorization();
 
             app.UseStaticFiles();
-       
-            
-           
-
-
             app.UseSession();
             app.UseMvc();
 
